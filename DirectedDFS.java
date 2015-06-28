02af3d1d04bc5c651a44eeb9d811812f4945bdc9
chenbowen <cikefengbao@163.com>
2015-06-28 14:51:01 +0800

    Create DirectedDFS.java

---
+++ DirectedDFS.java
@@ -0,0 +1,78 @@
+public class DirectedDFS {
+    private boolean[] marked;  // marked[v] = true if v is reachable
+                               // from source (or sources)
+    private int count;         // number of vertices reachable from s
+
+    public DirectedDFS(Digraph G, int s) {
+        marked = new boolean[G.V()];
+        dfs(G, s);
+    }
+
+    /**
+     * Computes the vertices in digraph <tt>G</tt> that are
+     * connected to any of the source vertices <tt>sources</tt>.
+     * @param G the graph
+     * @param sources the source vertices
+     */
+    public DirectedDFS(Digraph G, Iterable<Integer> sources) {
+        marked = new boolean[G.V()];
+        for (int v : sources) {
+            if (!marked[v]) dfs(G, v);
+        }
+    }
+
+    private void dfs(Digraph G, int v) { 
+        count++;
+        marked[v] = true;
+        for (int w : G.adj(v)) {
+            if (!marked[w]) dfs(G, w);
+        }
+    }
+
+    /**
+     * Is there a directed path from the source vertex (or any
+     * of the source vertices) and vertex <tt>v</tt>?
+     * @param v the vertex
+     * @return <tt>true</tt> if there is a directed path, <tt>false</tt> otherwise
+     */
+    public boolean marked(int v) {
+        return marked[v];
+    }
+
+    /**
+     * Returns the number of vertices reachable from the source vertex
+     * (or source vertices).
+     * @return the number of vertices reachable from the source vertex
+     *   (or source vertices)
+     */
+    public int count() {
+        return count;
+    }
+
+    /**
+     * Unit tests the <tt>DirectedDFS</tt> data type.
+     */
+    public static void main(String[] args) {
+
+        // read in digraph from command-line argument
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+
+        // read in sources from command-line arguments
+        Bag<Integer> sources = new Bag<Integer>();
+        for (int i = 1; i < args.length; i++) {
+            int s = Integer.parseInt(args[i]);
+            sources.add(s);
+        }
+
+        // multiple-source reachability
+        DirectedDFS dfs = new DirectedDFS(G, sources);
+
+        // print out vertices reachable from sources
+        for (int v = 0; v < G.V(); v++) {
+            if (dfs.marked(v)) StdOut.print(v + " ");
+        }
+        StdOut.println();
+    }
+
+}
